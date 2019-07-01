2713368ffb823bc0d521d05d99dfba1b8609e264
OliverPetri <oliverpetri81@yahoo.de>
2019-06-30 19:42:09 -0700

    sounds, reset and random generation

--- minesweeper.js
+++ minesweeper.js
@@ -2,30 +2,11 @@ document.addEventListener('DOMContentLoaded', startGame)
 
 // Define your `board` object here!
 var board = {
-  cells: [
-    { row: 0, col: 0, isMine: false, hidden: true },
-    { row: 0, col: 1, isMine: false, hidden: true },
-    { row: 0, col: 2, isMine: false, hidden: true },
-    { row: 0, col: 3, isMine: false, hidden: true },
-
-    { row: 1, col: 0, isMine: false, hidden: true },
-    { row: 1, col: 1, isMine: false, hidden: false },
-    { row: 1, col: 2, isMine: false, hidden: true },
-    { row: 1, col: 3, isMine: false, hidden: true },
-
-    { row: 2, col: 0, isMine: true, hidden: true },
-    { row: 2, col: 1, isMine: false, hidden: true },
-    { row: 2, col: 2, isMine: true, hidden: true },
-    { row: 2, col: 3, isMine: false, hidden: true },
-
-    { row: 3, col: 0, isMine: true, hidden: true },
-    { row: 3, col: 1, isMine: false, hidden: true },
-    { row: 3, col: 2, isMine: false, hidden: true },
-    { row: 3, col: 3, isMine: false, hidden: true }
-  ]
+  cells: []
 }
 
 function startGame () {
+  generateBoard()
   for (var i = 0; i < board.cells.length; i++) {
     var currentCell = board.cells[i]
     var surroundingMines = countSurroundingMines(currentCell)
@@ -34,11 +15,37 @@ function startGame () {
   // Don't remove this function call: it makes the game work!
   lib.initBoard()
   document.addEventListener('click', function (event) {
+    var audio = new Audio('sounds/select.wav');
+    audio.play();
     checkForWin()
   });
   document.addEventListener('contextmenu', function (event) {
+    var audio = new Audio('sounds/select.wav');
+    audio.play();
     checkForWin()
   });
+
+  document.getElementById('reset-button').addEventListener('click', function (event) {
+    resetBoard()
+  });
+}
+
+// Generates the cells of a 6x6 board
+// Uses a nested for loop to create the cells. A random number is generated to determine if the cell is a mine
+// All cells begin hidden
+function generateBoard () {
+  var boardSize = 6
+  for (var i = 0; i < boardSize; i++) {
+    for (var j = 0; j < boardSize; j++) {
+      var randomNumber = Math.random() * 10;
+      var isMine = false
+      if (randomNumber < 3)
+        isMine = true
+      var cell = { row: i, col: j, hidden: true, isMine: isMine }
+      board.cells.push(cell)
+    }
+  }
+
 }
 
 // Define this function to look for a win condition:
@@ -56,11 +63,13 @@ function checkForWin () {
       return
     }
 
-    if(currentCell.isMine === false && currentCell.hidden === true){
+    if (currentCell.isMine === false && currentCell.hidden === true) {
       return
     }
   }
 
+  var audio = new Audio('sounds/applause.wav');
+  audio.play();
   lib.displayMessage('You win!')
 }
 
@@ -83,5 +92,19 @@ function countSurroundingMines (cell) {
   }
 
   return surroundingCount
+}
+
+// Reset the board
+// Delete all child elements from the board element, after, hide and unmark all cells and finally call initboard
+function resetBoard () {
+  var boardNode = document.getElementsByClassName('board')[0]
+  while (boardNode.hasChildNodes()) {
+    boardNode.removeChild(boardNode.lastChild);
+  }
+  for (var i = 0; i < board.cells.length; i++) {
+    board.cells[i].isMarked = false
+    board.cells[i].hidden = true
+  }
+  lib.initBoard()
 }
 
